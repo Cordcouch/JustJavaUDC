fdd751f9e9332019e462f8ae78e7a9b9487b804b
Boris <boris.diener@sonomotors.com>
2019-09-02 13:16:29 +0200

    Add increase & decrease buttons

--- app/src/main/java/com/example/justjavaudc/MainActivity.java
+++ app/src/main/java/com/example/justjavaudc/MainActivity.java
@@ -1,13 +1,3 @@
-/**
- * IMPORTANT: Make sure you are using the correct package name.
- * This example uses the package name:
- * package com.example.android.justjava
- * If you get an error when copying this code into Android studio, update it to match teh package
- * name found
- * in the project's AndroidManifest.xml file.
- **/
-
-
 package com.example.justjavaudc;
 
 import android.os.Bundle;
@@ -23,6 +13,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,9 +25,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 2;
-        display(quantity);
-        displayPrice(quantity * 5);
+        int price = quantity * 5;
+        String priceMessage = "Total: $" + price;
+        priceMessage = priceMessage + "\nThank you!";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -52,5 +45,29 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+    /**
+     * This method is called when the increment button is clicked.
+     */
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the decrement button is clicked.
+     */
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        display(quantity);
     }
 }
